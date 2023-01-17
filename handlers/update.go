357aa69bf3c8886455d94cf75456ffd42fc7662e
Senrathy <sergioricardort@hotmail.com>
2023-01-17 06:50:26 -0300

    U from CRUD

---
+++ handlers/update.go
@@ -0,0 +1,18 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+	"github.com/go-chi/chi/v5"
+)
+
+func Update(w http.ResponseWriter, r *http.Request){
+	id, err := strconv.Atoi(chi .URLParam(r, "id"))
+
+	if err != nil {
+		log.Printf("Erro ao fazer decode do json: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+}
